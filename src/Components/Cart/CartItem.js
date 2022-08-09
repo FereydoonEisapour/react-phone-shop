0d55b783bd1579b1b68503380c9b39d1b17e9e95
Fereydoon <epfereydoon@gmail.com>
2022-08-09 13:43:44 +0000

    update Cart . add PropType

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -1,56 +1,69 @@
-import React, { useContext } from 'react'
-import ProductContext from '../../Contexts/products'
-import { Link } from 'react-router-dom'
-function Cart(props) {
-    let { img, title, price, count, } = props.products
-    const productContext = useContext(ProductContext)
-    let removeCart = () => {
-        productContext.dispatch({ type: 'remove_cart', payload: { id: props.products.id } })
-    }
-    let idHandler = () => {
-        productContext.dispatch({ type: 'id_handler', payload: { id: props.products.id } })
-    }
-    let increment = () => {
-        productContext.dispatch({ type: 'increment', payload: { id: props.products.id } })
-    }
-    let decrement = () => {
-        productContext.dispatch({ type: 'decrement', payload: { id: props.products.id } })
-    }
-    return (
-        <div className="container-fluid">
-            <div className="row col-12 d-flex justify-content-between shadow pt-3 mb-3" id="now">
-                <div className="col-3 ">
-                    <img className="card-img img-fluid w-100" src={img} alt="" />
-                </div>
-                <div className="col-9 d-flex row ">
-                    <div className="">
-                        <Link onClick={idHandler} to="/details" className="text-decoration-none" >
-                            <h4>{title}</h4>
-                        </Link>
-                    </div>
-                    <div className="">
-                        <h5 className="text-muted mt-5 "> Price : <span >{price}</span> $ </h5>
-                    </div>
-                    <div className="mt-3 mb-2">
-                        <button className="badge text-primary border-0  p-2 bg-white"
-                            style={{ fontSize: "1.2rem" }}
-                            onClick={count <= 1 ? removeCart : decrement}
-                        >-</button>
-                        <span className="h4 text-primary"> {count} </span>
-
-                        <button className="badge text-primary border-0  p-2 bg-white"
-                            style={{ fontSize: "1.2rem" }}
-                            onClick={increment} >+</button>
-                        <button className="btn btn-sm btn-danger ml-5"
-                            onClick={removeCart}
-                        >
-                            Remove
-                    </button>
-                    </div>
-                </div>
-            </div>
+import React, { useContext } from "react";
+import ProductContext from "../../Contexts/products";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+function Cart({ img, title, price, count, id }) {
+  const productContext = useContext(ProductContext);
+  let removeCart = () => {
+    productContext.dispatch({ type: "remove_cart", payload: { id } });
+  };
+  let idHandler = () => {
+    productContext.dispatch({ type: "id_handler", payload: { id } });
+  };
+  let increment = () => {
+    productContext.dispatch({ type: "increment", payload: { id } });
+  };
+  let decrement = () => {
+    productContext.dispatch({ type: "decrement", payload: { id } });
+  };
+  return (
+    <div className="container-fluid">
+      <div className="row col-12 d-flex justify-content-between shadow pt-3 mb-3" id="now">
+        <div className="col-3 ">
+          <img className="card-img img-fluid w-100" src={img} alt="" />
         </div>
-    )
-}
+        <div className="col-9 d-flex row ">
+          <div className="">
+            <Link onClick={idHandler} to="/details" className="text-decoration-none">
+              <h4>{title}</h4>
+            </Link>
+          </div>
+          <div className="">
+            <h5 className="text-muted mt-5 ">
+              Price : <span>{price}</span> $
+            </h5>
+          </div>
+          <div className="mt-3 mb-2">
+            <button
+              className="badge text-primary border-0  p-2 bg-white"
+              style={{ fontSize: "1.2rem" }}
+              onClick={count <= 1 ? removeCart : decrement}
+            >
+              -
+            </button>
+            <span className="h4 text-primary"> {count} </span>
 
-export default Cart
+            <button
+              className="badge text-primary border-0  p-2 bg-white"
+              style={{ fontSize: "1.2rem" }}
+              onClick={increment}
+            >
+              +
+            </button>
+            <button className="btn btn-sm btn-danger ml-5" onClick={removeCart}>
+              Remove
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+Cart.propTypes = {
+  id: PropTypes.number,
+  img: PropTypes.string,
+  title: PropTypes.string,
+  count: PropTypes.number,
+  price: PropTypes.number,
+};
+export default Cart;
--- src/Components/Cart/CartList.js
+++ src/Components/Cart/CartList.js
@@ -1,20 +1,32 @@
-import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
-import ProductContext from '../../Contexts/products'
-import CartItem from './CartItem'
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import ProductContext from "../../Contexts/products";
+import CartItem from "./CartItem";
 function CartList() {
-    const productContext = useContext(ProductContext)
-    let inCartsProducts = productContext.tempProducts.filter(item=>item.inCart === true)
-    return (
-        <>
-            {
-                inCartsProducts.length === 0 ?
-                    <h1 style={{ padding: '100px' }}>No Product in Cart ,  <Link to="/">add Product</Link></h1>
-                  
-                    : inCartsProducts.map(products => <CartItem key={products.id} products={products} />)
-            }
-        </>
-    )
+  const productContext = useContext(ProductContext);
+  let inCartsProducts = productContext.tempProducts.filter((item) => item.inCart === true);
+  return (
+    <>
+      {inCartsProducts.length === 0 ? (
+        <h1 style={{ padding: "100px" }}>
+          No Product in Cart , <Link to="/">add Product</Link>
+        </h1>
+      ) : (
+        inCartsProducts.map((products) => (
+          <CartItem
+            key={products.id}
+            //   products={products}
+            // img, title, price, count, id
+            img={products.img}
+            title={products.title}
+            price={products.price}
+            count={products.count}
+            id={products.id}
+          />
+        ))
+      )}
+    </>
+  );
 }
 
-export default CartList
+export default CartList;
